462f49ed29165039768a6570c74272f12e20283b
+dorcus <+wayumulu58@gmail.com>
2022-06-09 14:23:55 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  const initialPoint = 42
+  const ReturnVal = Math.abs(someValue - initialPoint);
+  return ReturnVal;
+}
+
+
+function distanceFromHqInFeet(someValue) {
+ const initialPoint = 42;
+ const ReturnVal = Math.abs(someValue-initialPoint) * 264;
+
+ return ReturnVal;
+}
+
+ function distanceTravelledInFeet(start, destination) {  
+       return Math.abs((destination - start) * 264);
+ }
+
+  function calculatesFarePrice(start, destination) {
+    let distance  = destination-start *264;
+
+    if(distance < 400){
+        return "cannot travel that far";
+    }else if(distance-400 > 0 && distance < 2000){
+     return (distance-400) * 2;
+    }
+  }
